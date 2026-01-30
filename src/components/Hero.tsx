d378213f45d09e2408de26f363177345e8592680
Michaelmwaringa <acadeemiasms@gmail.com>
2026-01-30 16:44:57 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
         className="absolute inset-0 w-full h-full object-cover"
       >
         <source
-          src="https://videos.pexels.com/video-files/5745860/5745860-hd_1280_720_24fps.mp4"
+          src="https://videos.pexels.com/video-files/30841347/30841347-hd_1280_720_24fps.mp4"
           type="video/mp4"
         />
       </video>
